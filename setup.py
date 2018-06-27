5b480eb4632a1723f88a2171d058ac400a47fa81
Yugantar Malhotra <yugantarmalhotra95@gmail.com>
2018-06-27 19:14:41 +0530

    Update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='cricapi-py',
     version='1.0.0',
     description='Simple and Easy way to access CricAPI data. '
                 'A library showing live data, upcoming matches, fantasy summary and more',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/yugantar7/py-cricapi',
     author='Yugantar Malhotra',
     author_email='yugantarmalhotra95@gmail.com',
     package_dir={'': 'src'},
